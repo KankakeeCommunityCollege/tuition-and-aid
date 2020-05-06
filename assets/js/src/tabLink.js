309e3b23ef84ec29ea0b88c67900750a945828b4
wdzajicek <tig.welder4@gmail.com>
2020-05-06 16:15:41 -0500

    Add and remove comments from JS module.

--- assets/js/src/tabLink.js
+++ assets/js/src/tabLink.js
@@ -1,11 +1,10 @@
-// Provide the ability to activate a tab from the URL (e.g. 'http://localhost:3000/alumni/distinguished-alumni-award/#donald-rehmer' makes the #donald-rehmer tab active).
 const FINANCIAL_AID_PAGE_HASH_LINK = /(\/financial-aid\/#|\/financial-aid#)/g;
 
 function checkForMatchingTabOrAccordion(hash) {
   if ( document.querySelector('#financial-aid a[href="' + hash.replace(/\/$/g, '') + '"]') ) {
-    $('#financial-aid a[href="' + hash.replace(/\/$/g, '') + '"]').tab('show');
+    $('#financial-aid a[href="' + hash.replace(/\/$/g, '') + '"]').tab('show'); // Bootstrap 4 Tab method
   } else if ( document.querySelector(hash.replace(/\/$/g, '') + '.collapse') ) {
-    $(hash.replace(/\/$/g, '')).collapse('show');
+    $(hash.replace(/\/$/g, '')).collapse('show'); // Bootstrap 4 Collapse method
   }
 }
 
