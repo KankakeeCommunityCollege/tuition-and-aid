02dfa9367e10dcae25abc9dc0bef4e47fc5c743e
wdzajicek <tig.welder4@gmail.com>
2021-05-06 16:30:33 -0500

    Import netPriceCalcDarkMode.js statically.

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -1,9 +1,9 @@
 //import './babelTest.js';
+import netPriceCalcDarkMode from './netPriceCalcDarkMode.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-  if (window.location.pathname.search(/\/net-price-calculator\//g) !== -1 ) {
-    import(/* webpackChunkName: 'netPriceCalcDarkMode' */ './netPriceCalcDarkMode').then(({default: netPriceCalcDarkMode}) => {
-      netPriceCalcDarkMode();
-    });
-  }
+  if (window.location.pathname.search(/\/net-price-calculator\//g) === -1)
+    return;
+  
+  netPriceCalcDarkMode();
 });
