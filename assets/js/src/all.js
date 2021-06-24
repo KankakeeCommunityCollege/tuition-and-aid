f3ee42fbea37f2974d3cbaa1a2764bcce3257525
wdzajicek <tig.welder4@gmail.com>
2021-06-23 19:56:50 -0500

    Add missing localStorage `darkModeSetting` to import conditions.

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -1,7 +1,10 @@
 import '../../scss/main.scss';
 
 document.addEventListener('DOMContentLoaded', function() {
-  if (window.location.pathname.search(/\/net-price-calculator\//g) !== -1) {
+  if (
+    window.location.pathname.search(/\/net-price-calculator\//g) !== -1 &&
+    window.localStorage.getItem('darkModeSetting') == 'true'
+  ) {
     import(/* webpackChunkName: 'netPriceCalcDarkMode' */ './netPriceCalcDarkMode').then(({default: netPriceCalcDarkMode}) => {
       netPriceCalcDarkMode();
     });
