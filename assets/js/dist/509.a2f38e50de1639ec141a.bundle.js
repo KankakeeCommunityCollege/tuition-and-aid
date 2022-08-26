6c5564d052339e752935145852b4f21c056c4f49
wdzajicek <tig.welder4@gmail.com>
2022-08-26 12:29:34 -0500

    Add banner. Update browserslist + vulnerable packages...
    - Update the browserslist database (and create new build files.)
    - Update vulnerable version of `sass-loader` to latest.
    - Add payment deadline banner to tuition-and-aid homepage &
      financial-aid page.
    - Add comments to main custom JS entry point file.

---
+++ assets/js/dist/509.a2f38e50de1639ec141a.bundle.js
@@ -0,0 +1,1 @@
+(self.webpackChunkkcc_tuition_and_aid=self.webpackChunkkcc_tuition_and_aid||[]).push([[509],{1509:function(e,t,n){"use strict";function c(e){var t=e.target.contentWindow.document.body.querySelector(".theme-light");t.classList.add("theme-dark"),t.classList.remove("theme-light")}n.r(t),t.default=function(){document.getElementById("netPriceIframe").addEventListener("load",c)}}}]);
