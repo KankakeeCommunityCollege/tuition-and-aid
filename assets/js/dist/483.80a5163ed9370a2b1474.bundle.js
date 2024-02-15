c302e1dd6a93f8690b6a8f7b020f8dbd9dc79597
wdzajicek <tig.welder4@gmail.com>
2024-02-15 10:34:54 -0600

    Upgrade Nodejs to `v20.11.1`.

---
+++ assets/js/dist/483.80a5163ed9370a2b1474.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkcc_tuition_and_aid=self.webpackChunkkcc_tuition_and_aid||[]).push([[483],{483:function(e,t,n){function c(e){const t=e.target.contentWindow.document.body.querySelector(".theme-light");t.classList.add("theme-dark"),t.classList.remove("theme-light")}n.r(t),t.default=function(){document.getElementById("netPriceIframe").addEventListener("load",c)}}}]);
--- assets/js/dist/509.ec729180c58b7f0fb623.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkcc_tuition_and_aid=self.webpackChunkkcc_tuition_and_aid||[]).push([[509],{509:function(e,t,n){function c(e){const t=e.target.contentWindow.document.body.querySelector(".theme-light");t.classList.add("theme-dark"),t.classList.remove("theme-light")}n.r(t),t.default=function(){document.getElementById("netPriceIframe").addEventListener("load",c)}}}]);
